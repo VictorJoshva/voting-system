98f1868eda0770a7a1ef3a5f66e7c1e62912e370
Firstname Lastname <your_email@youremail.com>
2022-06-27 00:08:05 +0530

    first commit

---
+++ includes/footer.php
@@ -0,0 +1,9 @@
+<footer class="main-footer">
+    <div class="container">
+      <div class="pull-right hidden-xs">
+        <b>Voting System</b>
+      </div>
+      <strong> </a></strong>
+    </div>
+    <!-- /.container -->
+</footer>
